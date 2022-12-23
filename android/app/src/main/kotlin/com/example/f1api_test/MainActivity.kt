9227ba047b6e5d03c0da3262ae32bddf1547cc9f
SONU <srishyama01@gmail.com>
2022-12-23 18:35:52 +0530

    simple data feach from api

---
+++ android/app/src/main/kotlin/com/example/f1api_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f1api_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter api',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyApi(),
+    );
+  }
+}
+class MyApi extends StatefulWidget {
+  const MyApi({super.key});
+
+  @override
+  State<MyApi> createState() => _MyApiState();
+}
+
+class _MyApiState extends State<MyApi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: 
+       const HomePage(),
+    );
+  }
+}
+
