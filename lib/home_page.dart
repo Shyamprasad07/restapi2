d60097f78c5637184e470159756d7199fb4f9326
SONU <srishyama01@gmail.com>
2022-12-23 20:35:38 +0530

    nested address  data feach from api

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,7 +4,6 @@ import 'package:f1api_test/model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -13,76 +12,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<SamplePosts>samplePosts = [];
+  List<UserDetails> userDetails = [];
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: getData(),
       builder: (context, snapshot) {
-        if (snapshot.hasData){
+        if (snapshot.hasData) {
           return ListView.builder(
-          itemCount: samplePosts.length,
-          itemBuilder: (context, index) {
-            return Card(
-              child: Container(
-                height: 300,
-                width: 300,
-                color: Colors.greenAccent,
-                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            itemCount: userDetails.length,
+            itemBuilder: (context, index) {
+              return Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                margin: const EdgeInsets.only(bottom: 10),
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(10)),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      'user id : ${samplePosts[index].userId}',
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                    Text(
-                      'Id : ${samplePosts[index].id}',
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                    Text(
-                      'Title : ${samplePosts[index].title}',
-                    maxLines: samplePosts.length,
-                      style: const TextStyle(fontSize: 18),
-                    ),
-                    Text(
-                      'Body : ${samplePosts[index].body}',
-                      maxLines:samplePosts.length,
-                      style: const TextStyle(fontSize: 18),
-                    ),
+                    getText(index, 'ID: ', userDetails[index].id.toString()),
+                    getText(
+                        index, 'Name: ', userDetails[index].name.toString()),
+                    getText(
+                        index, 'Email: ', userDetails[index].email.toString()),
+                    getText(
+                        index, 'Phone: ', userDetails[index].phone.toString()),
+                    getText(index, 'Website: ',
+                        userDetails[index].website.toString()),
+                    getText(index, 'Company: ',
+                        userDetails[index].company.name.toString()),
+                    getText(index, 'Address: ',
+                        '${userDetails[index].address.suite.toString()},${userDetails[index].address.street.toString()},${userDetails[index].address.city.toString()} - ${userDetails[index].address.zipcode.toString()},'),
                   ],
                 ),
-              ),
-            );
-          },
-        );
-
-        }
-        else {
-          return  const Center(
+              );
+            },
+          );
+        } else {
+          return const Center(
             child: CircularProgressIndicator(),
           );
         }
-      }
-      
+      },
     );
   }
 
-  Future<List<SamplePosts>> getData() async {
-    final response =
-        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
-         var data = jsonDecode(response.body.toString());
-         if (response.statusCode ==200){
-          for(Map<String, dynamic> index in data){
-            samplePosts.add(SamplePosts.fromJson(index));
-          }
-            return samplePosts;
-          } else{
-            return samplePosts;
-          }
-         }
+  Text getText(int index, String fieldName, String content) {
+    return Text.rich(TextSpan(children: [
+      TextSpan(
+          text: fieldName,
+          style: (const TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+      TextSpan(text: content, style: (const TextStyle(fontSize: 16))),
+    ]));
   }
 
+  Future<List<UserDetails>> getData() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> index in data) {
+        userDetails.add(UserDetails.fromJson(index));
+      }
+      return userDetails;
+    } else {
+      return userDetails;
+    }
+  }
+}
--- lib/model.dart
+++ lib/model.dart
@@ -1,37 +1,129 @@
 // To parse this JSON data, do
 //
-//     final samplePosts = samplePostsFromJson(jsonString);
+//     final userDetails = userDetailsFromJson(jsonString);
 
 import 'dart:convert';
 
-List<SamplePosts> samplePostsFromJson(String str) => List<SamplePosts>.from(json.decode(str).map((x) => SamplePosts.fromJson(x)));
+List<UserDetails> userDetailsFromJson(String str) => List<UserDetails>.from(json.decode(str).map((x) => UserDetails.fromJson(x)));
 
-String samplePostsToJson(List<SamplePosts> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String userDetailsToJson(List<UserDetails> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-class SamplePosts {
-    SamplePosts({
-        required this.userId,
+class UserDetails {
+    UserDetails({
         required this.id,
-        required this.title,
-        required this.body,
+        required this.name,
+        required this.username,
+        required this.email,
+        required this.address,
+        required this.phone,
+        required this.website,
+        required this.company,
     });
 
-    int userId;
     int id;
-    String title;
-    String body;
+    String name;
+    String username;
+    String email;
+    Address address;
+    String phone;
+    String website;
+    Company company;
 
-    factory SamplePosts.fromJson(Map<String, dynamic> json) => SamplePosts(
-        userId: json["userId"],
+    factory UserDetails.fromJson(Map<String, dynamic> json) => UserDetails(
         id: json["id"],
-        title: json["title"],
-        body: json["body"],
+        name: json["name"],
+        username: json["username"],
+        email: json["email"],
+        address: Address.fromJson(json["address"]),
+        phone: json["phone"],
+        website: json["website"],
+        company: Company.fromJson(json["company"]),
     );
 
     Map<String, dynamic> toJson() => {
-        "userId": userId,
         "id": id,
-        "title": title,
-        "body": body,
+        "name": name,
+        "username": username,
+        "email": email,
+        "address": address.toJson(),
+        "phone": phone,
+        "website": website,
+        "company": company.toJson(),
+    };
+}
+
+class Address {
+    Address({
+        required this.street,
+        required this.suite,
+        required this.city,
+        required this.zipcode,
+        required this.geo,
+    });
+
+    String street;
+    String suite;
+    String city;
+    String zipcode;
+    Geo geo;
+
+    factory Address.fromJson(Map<String, dynamic> json) => Address(
+        street: json["street"],
+        suite: json["suite"],
+        city: json["city"],
+        zipcode: json["zipcode"],
+        geo: Geo.fromJson(json["geo"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "street": street,
+        "suite": suite,
+        "city": city,
+        "zipcode": zipcode,
+        "geo": geo.toJson(),
+    };
+}
+
+class Geo {
+    Geo({
+        required this.lat,
+        required this.lng,
+    });
+
+    String lat;
+    String lng;
+
+    factory Geo.fromJson(Map<String, dynamic> json) => Geo(
+        lat: json["lat"],
+        lng: json["lng"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "lat": lat,
+        "lng": lng,
+    };
+}
+
+class Company {
+    Company({
+        required this.name,
+        required this.catchPhrase,
+        required this.bs,
+    });
+
+    String name;
+    String catchPhrase;
+    String bs;
+
+    factory Company.fromJson(Map<String, dynamic> json) => Company(
+        name: json["name"],
+        catchPhrase: json["catchPhrase"],
+        bs: json["bs"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "name": name,
+        "catchPhrase": catchPhrase,
+        "bs": bs,
     };
 }
